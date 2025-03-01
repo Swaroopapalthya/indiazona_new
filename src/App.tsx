63e4ee72a131cd69d46885516351181baeb0c15a
Swaroopapalthya <166479095+Swaroopapalthya@users.noreply.github.com>
2025-03-01 13:42:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { ThemeProvider } from '@mui/material/styles';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import { theme } from './theme/theme';
-// import Layout from './components/layout/Layout';
-// import Dashboard from './pages/Dashboard';
-// import Sellers from './pages/Sellers';
 
-// function App() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       <Router>
-//         <Layout>
-//           <Routes>
-//             <Route path="/" element={<Dashboard />} />
-//             <Route path="/" element={<Dashboard />} />
-//             <Route path="/sellers" element={<Sellers />} />
-//             {/* Add more routes here as we create more pages */}
-//           </Routes>
-//         </Layout>
-//       </Router>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
-
-
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
